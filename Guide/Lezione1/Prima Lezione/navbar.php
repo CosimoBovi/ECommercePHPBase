c3f6980ab3ab9f68bafda9ae0f927031d4bc3622
CosimoBovi <90317089+CosimoBovi@users.noreply.github.com>
2023-11-06 15:51:56 +0100

    Add files via upload

---
+++ Guide/Lezione1/Prima Lezione/navbar.php
@@ -0,0 +1,17 @@
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark ">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="index.php">E-Commerce</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse " id="collapsibleNavbar">
+        <ul class="navbar-nav me-auto">
+          
+          <li class="nav-item">
+            <a class="nav-link" href="index.php">Home</a>
+          </li>
+        
+        </ul>
+      </div>
+    </div>
+  </nav>
