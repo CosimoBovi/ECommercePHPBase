d8be88057192ef84fc078127ea5fe79549f3e630
CosimoBovi <90317089+CosimoBovi@users.noreply.github.com>
2023-11-16 13:57:12 +0100

    Add files via upload

---
+++ Guide/Lezione6/Codice Lezione 6/Control/userControl.php
@@ -0,0 +1,58 @@
+<?php
+    // Includi il file del modello utente (userModel.php).
+    require_once "../Model/userModel.php";
+
+    // Leggi i dati inviati dalla richiesta HTTP (in formato JSON) e convertili in un array associativo.
+    $Dati = json_decode(file_get_contents('php://input'), true);
+
+    // Verifica l'azione specificata nei dati.
+    if ($Dati["action"] == "login") {
+        // Se l'azione è "login," chiama la funzione "login" dal modello utente con l'username e la password forniti.
+        
+        $errore = login($Dati["username"], $Dati["password"]);
+        echo json_encode(['errore'=>$errore]);
+    }
+
+    if ($Dati["action"] == "userInfo") {
+        
+        $risultato = testLogin();
+        echo json_encode(['username'=>$risultato]);
+
+
+    }
+
+    if(isset($_GET["logout"])){
+        session_start();
+        session_destroy();
+        header('location: ../index.php?logout');
+        exit();
+    }
+
+
+    if ($Dati["action"] == "register") {
+        // Leggi i dati di registrazione inviati dalla richiesta HTTP
+        $username = $Dati["username"];
+        $mail = $Dati["mail"];
+        $password = $Dati["password"];
+        $usertype = $Dati["usertype"];
+        
+        // Effettua la registrazione dell'utente
+        $registrationStatus = insertUser($username, $mail, $password, $usertype);
+        
+        // Restituisci lo stato della registrazione come risposta JSON
+        echo json_encode(['registrationStatus' => $registrationStatus]);
+    }
+
+    if ($Dati["action"] == "getUserTypes") {
+        // Ottieni gli elementi dalla tabella UserTypes
+        $userTypes = getAllUserTypes();
+        
+        if ($userTypes !== false) {
+            // Restituisci tutti gli elementi come risposta JSON
+            echo json_encode($userTypes);
+        } else {
+            // Restituisci un messaggio di errore o un valore predefinito
+            echo json_encode(['error' => 'Failed to retrieve user types']);
+        }
+    }
+?>
---
+++ Guide/Lezione6/Codice Lezione 6/Model/userModel.php
@@ -0,0 +1,110 @@
+
+<?php
+// Inizializzazione della sessione
+session_start();
+
+// La funzione login prende due parametri: $user e $pass, che rappresentano rispettivamente l'username e la password dell'utente.
+
+function login($user, $pass) {
+    $servername = "localhost";
+    $dbname = "ecommercedb";
+    $dbusername = "root";
+    $dbpassword = "";
+
+    try {
+        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $dbusername, $dbpassword);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        $stmt = $conn->prepare("SELECT Username, Password FROM Users WHERE Username = :username");
+        $stmt->bindParam(':username', $user);
+        $stmt->execute();
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if (!$result) {
+            return 2; // Utente non esiste
+        }
+
+        $hashedPassword = $result['Password'];
+
+        if (password_verify($pass, $hashedPassword)) {
+            $_SESSION["username"]=$user;
+            return 0; // Accesso riuscito
+        } else {
+            return 3; // Password errata
+        }
+    } catch(PDOException $e) {
+        return 1; // Errore generico
+    }
+}
+
+function testLogin(){
+    if(isset($_SESSION["username"])){
+        return $_SESSION["username"];
+    }else{
+        return "";
+    }
+}
+
+function insertUser($username, $mail, $password, $usertype) {
+    $servername = "localhost";
+    $dbname = "ecommercedb";
+    $dbusername = "root";
+    $dbpassword = "";
+
+    try {
+        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $dbusername, $dbpassword);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        // Hash della password prima di salvarla nel database
+        $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+
+        // Query per l'inserimento dell'utente
+        $sql = "INSERT INTO Users (Username, Mail, Password, Usertypeid) VALUES (:username, :mail, :password, :usertype)";
+        $stmt = $conn->prepare($sql);
+        $stmt->bindParam(':username', $username);
+        $stmt->bindParam(':mail', $mail);
+        $stmt->bindParam(':password', $hashedPassword);
+        $stmt->bindParam(':usertype', $usertype);
+
+        $stmt->execute();
+
+        return 0; // Ritorna 0 se l'inserimento è avvenuto con successo
+    } catch(PDOException $e) {
+        $errorCode = $e->getCode();
+
+        // Verifica il codice di errore per violazione delle chiavi univoche
+        if ($errorCode === '23000' || $errorCode === 1062) {
+            $errorMessage = $e->getMessage();
+
+            if (strpos($errorMessage, 'Duplicate entry') !== false && strpos($errorMessage, 'Username') !== false) {
+                return 2; // Codice di errore per username già esistente
+            } elseif (strpos($errorMessage, 'Duplicate entry') !== false && strpos($errorMessage, 'Mail') !== false) {
+                return 3; // Codice di errore per email già esistente
+            }
+        } else {
+            return 1; // Errore generico
+        }
+    }
+}
+
+function getAllUserTypes() {
+    $servername = "localhost";
+    $dbname = "ecommercedb";
+    $username = "root";
+    $password = "";
+
+    try {
+        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        $stmt = $conn->prepare("SELECT * FROM UserTypes WHERE type != 'Administrator'");
+        $stmt->execute();
+
+        $userTypes = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        return $userTypes;
+    } catch(PDOException $e) {
+        // Gestione degli errori
+        return false;
+    }
+}
+?>
